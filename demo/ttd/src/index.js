c3833e06fce7018b7e5b4375f5e4474a2b5f5adb
Leif Anderson <leif.v.anderson@gmail.com>
2018-10-12 23:21:02 -0500

    added suit and sums to board

--- demo/ttd/src/index.js
+++ demo/ttd/src/index.js
@@ -6,11 +6,12 @@ import Deck from './decklib';
 const deck = new Deck();
 deck.generate_deck();
 deck.shuffle();
+deck.deal_hands();
 
-class Square extends React.Component {
+class Card extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="card">
         {this.props.value}
       </button>
     );
@@ -18,8 +19,8 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  renderCard(n) {
+    return <Card value={n} />;
   }
 
   render() {
@@ -29,64 +30,73 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
+          {this.renderCard(deck.hands[0][0].name)}
+          {this.renderCard(deck.hands[0][1].name)}
+          {this.renderCard(deck.hands[0][2].name)}
+          {this.renderCard(deck.hands[0][3].name)}
+          {this.renderCard(deck.hands[0][4].name)}
+          {this.renderCard(deck.hands[0][5].name)}
+          {this.renderCard(deck.hands[0][6].name)}
+          {this.renderCard(deck.hands[0][7].name)}
+          {this.renderCard(deck.hands[0][8].name)}
+          {this.renderCard(deck.hands[0][9].name)}
+          {this.renderCard(deck.hands[0][10].name)}
+          {this.renderCard(deck.hands[0][11].name)}
+          {this.renderCard(deck.hands[0][12].name)}
+          {this.renderCard(" ")}
+          {this.renderCard(deck.sum_majors(0))}          
         </div>
         <div className="board-row">
-        {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
+          {this.renderCard(deck.hands[1][0].name)}
+          {this.renderCard(deck.hands[1][1].name)}
+          {this.renderCard(deck.hands[1][2].name)}
+          {this.renderCard(deck.hands[1][3].name)}
+          {this.renderCard(deck.hands[1][4].name)}
+          {this.renderCard(deck.hands[1][5].name)}
+          {this.renderCard(deck.hands[1][6].name)}
+          {this.renderCard(deck.hands[1][7].name)}
+          {this.renderCard(deck.hands[1][8].name)}
+          {this.renderCard(deck.hands[1][9].name)}
+          {this.renderCard(deck.hands[1][10].name)}
+          {this.renderCard(deck.hands[1][11].name)}
+          {this.renderCard(deck.hands[1][12].name)}
+          {this.renderCard(" ")}
+          {this.renderCard(deck.sum_majors(1))}
         </div>
         <div className="board-row">
-        {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
+          {this.renderCard(deck.hands[2][0].name)}
+          {this.renderCard(deck.hands[2][1].name)}
+          {this.renderCard(deck.hands[2][2].name)}
+          {this.renderCard(deck.hands[2][3].name)}
+          {this.renderCard(deck.hands[2][4].name)}
+          {this.renderCard(deck.hands[2][5].name)}
+          {this.renderCard(deck.hands[2][6].name)}
+          {this.renderCard(deck.hands[2][7].name)}
+          {this.renderCard(deck.hands[2][8].name)}
+          {this.renderCard(deck.hands[2][9].name)}
+          {this.renderCard(deck.hands[2][10].name)}
+          {this.renderCard(deck.hands[2][11].name)}
+          {this.renderCard(deck.hands[2][12].name)}
+          {this.renderCard(" ")}
+          {this.renderCard(deck.sum_majors(2))}          
         </div>
         <div className="board-row">
-        {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
-          {this.renderSquare(deck.deal().value)}
+          {this.renderCard(deck.hands[3][0].name)}
+          {this.renderCard(deck.hands[3][1].name)}
+          {this.renderCard(deck.hands[3][2].name)}
+          {this.renderCard(deck.hands[3][3].name)}
+          {this.renderCard(deck.hands[3][4].name)}
+          {this.renderCard(deck.hands[3][5].name)}
+          {this.renderCard(deck.hands[3][6].name)}
+          {this.renderCard(deck.hands[3][7].name)}
+          {this.renderCard(deck.hands[3][8].name)}
+          {this.renderCard(deck.hands[3][9].name)}
+          {this.renderCard(deck.hands[3][10].name)}
+          {this.renderCard(deck.hands[3][11].name)}
+          {this.renderCard(deck.hands[3][12].name)}
+          {this.renderCard(" ")}          
+          {this.renderCard(deck.sum_majors(3))}
+          {this.renderCard(deck.sum_all_majors())}         
         </div>
       </div>
     );
