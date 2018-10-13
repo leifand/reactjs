ae0dfbfccdcd4764cabb3bbbebac6b822f16e71d
Leif Anderson <leif.v.anderson@gmail.com>
2018-10-12 23:17:50 -0500

    added hands and sums

--- demo/ttd/src/decklib.js
+++ demo/ttd/src/decklib.js
@@ -2,20 +2,21 @@ module.exports = class Deck {
     constructor() {
         this.deck = [];
         this.dealt_cards = [];
+        this.hands = [];
     };
 
     generate_deck() {
-        let card = (suit,value) => {
-            this.name = value+' of '+suit;
+        let card = (suit,rank) => {
+            this.name = rank+suit;
             this.suit = suit;
-            this.value = value;
-            return {'name':this.name,'suit':this.suit,'value':this.value};
+            this.rank = rank;
+            return {'name':this.name,'suit':this.suit,'rank':this.rank};
         };
-        let values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-        let suits = ['Clubs','Diamonds','Spades','Hearts'];
+        let ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+        let suits = ['C','D','S','H'];
         for(let s=0; s < suits.length; s++) {
-            for(let v=0; v < values.length; v++) {
-                this.deck.push(card(suits[s],values[v]));
+            for(let v=0; v < ranks.length; v++) {
+                this.deck.push(card(suits[s],ranks[v]));
             }
         }
         return this.deck;
@@ -43,6 +44,32 @@ module.exports = class Deck {
         let dealtcard = this.deck.shift();
         this.dealt_cards.push(dealtcard);
         return dealtcard;
+    }
+
+    deal_hands() {
+        let hand = [];
+        for (let i=0; i<4; i++) {
+            for (let j=0; j<13; j++) hand.push(this.deal());
+            this.hands.push(hand);
+            hand = [];
+        }
+        return this.hands;
+    }
+
+    sum_majors(n) {
+        let sum=0;
+        for(let i=0; i<13; i++) {
+            if (this.hands[n][i].rank === 'A') sum += 4;
+            else if (this.hands[n][i].rank === 'K') sum += 3;
+            else if (this.hands[n][i].rank === 'Q') sum+= 2;
+            else if (this.hands[n][i].rank === 'J') sum += 1;
+        }
+        return sum;
+    }
+
+    sum_all_majors() {
+        let sum = this.sum_majors(0) + this.sum_majors(1) + this.sum_majors(2) + this.sum_majors(3);
+        return sum;
     }
 
     replace() {
